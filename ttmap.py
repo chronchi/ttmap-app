bc009a0cbffb2baecf8a4e741354124c46a2329e
chronchi <carloshvronchi@gmail.com>
2020-04-23 14:17:05 +0200

    move everything to ttmap.py

--- ttmap.py
+++ ttmap.py
@@ -30,11 +30,62 @@ import panel as pn
 
 hv.extension('bokeh')
 
-sys.path.append('code')
-from utils import *
-from widgets import *
-from plot_functions import *
-from howtouse import *
+class simple_ttmap_output:
+    def __init__(self, ttmap_output, deviation_by_sample, samples_name, total_deviation):
+        self.ttmap_output = ttmap_output
+        self.deviation_by_sample = deviation_by_sample
+        self.samples_name = samples_name
+        self.total_deviation = total_deviation
+
+
+def run_simpleTTMap(dataset,
+                    output_directory = '.',
+                    alpha            = 1.0,
+                    batches          = [],
+                    test_samples     = [],
+                    outlier_value    = 1.0
+                   ):
+    """
+    Run simpleTTMap from R and format its outputs.
+    """    
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        r_dataset = ro.conversion.py2rpy(dataset)
+
+    col_names = [col_name[1:] if 'X' == col_name[0] else col_name for col_name in list(robjects.r['colnames'](r_dataset))]
+    r_dataset.colnames = rpy2.robjects.StrVector(col_names)
+    
+    simp_ttmap_output = simpletTTMap.simple_ttmap(r_dataset,
+                                                    output_directory  = output_directory,
+                                                    alpha             = alpha,
+                                                    batches           = batches,
+                                                    test_samples      = test_samples,
+                                                    outlier_parameter = outlier_value
+                                                )
+
+    py_simple_ttmap_output = dict(zip(simp_ttmap_output.names, simp_ttmap_output))
+
+    samples_name_rpy2 = list(py_simple_ttmap_output['samples_name'].columns[3:])
+
+    ttmap_output_rpy2 = dict(zip(py_simple_ttmap_output['ttmap_gtlmap'].names, map(list, list(py_simple_ttmap_output['ttmap_gtlmap']))))
+
+    for intervals in ttmap_output_rpy2:
+        for counter, clusters in enumerate(ttmap_output_rpy2[intervals]):
+            ttmap_output_rpy2[intervals][counter] = list(clusters)
+    
+    deviation_components_rpy2 = py_simple_ttmap_output['ttmap_hda'] 
+
+    deviation_components_rpy2 = dict(zip(deviation_components_rpy2.names, 
+                                         map(list,list(deviation_components_rpy2))))
+
+    total_deviations_rpy2 = deviation_components_rpy2['m']
+
+    deviation_by_sample_rpy2 = {}
+
+    for counter, sample_name in enumerate(samples_name_rpy2):
+        deviation_by_sample_rpy2[sample_name] = total_deviations_rpy2[counter]
+
+    return simple_ttmap_output(ttmap_output_rpy2, deviation_by_sample_rpy2, samples_name_rpy2, total_deviations_rpy2)
+
 
 def get_nodes_and_edges(sttmap_output, 
                         deviation_by_sample,
@@ -130,6 +181,126 @@ def get_nodes_and_edges(sttmap_output,
                            'clusters'     : clusters}
     
     return node_and_edges_data
+
+def hook_graph(plot, element):
+    """ 
+    Create hook function to add colorbar and other elements to the graph
+    """
+    data_source = plot.handles['scatter_1_source'].data
+    
+    node_data_in_graph = data_source
+    
+    # create color bar to append on the left
+    color_mapper_graph = bkm.LogColorMapper(palette="Viridis256", 
+                                            low=np.min(node_data_in_graph['node_color']),
+                                            high=np.max(node_data_in_graph['node_color']))
+    colorbar_graph = bkm.ColorBar(color_mapper=color_mapper_graph, location=(0,0), title = 'Deviation')
+
+    fig = hv.render(plot)
+    fig.add_layout(colorbar_graph, 'left')
+    
+    plot.handles['plot'].sizing_mode = 'stretch_both'
+    
+    
+def get_graph_from_data(node_and_edges_data):
+    
+    node_data    = node_and_edges_data['node_data']
+    source_edges = node_and_edges_data['source_edges']
+    target_edges = node_and_edges_data['target_edges']
+    
+    nodes = hv.Nodes(data=node_data)
+    
+    tooltips = [('Samples', '@samples'), ('Mean Deviation', '@mean_deviation'), ('Index', '@index')]
+    hover = HoverTool(tooltips = tooltips)
+    
+    # add labels to nodes 
+    labels = hv.Labels(nodes, ['x', 'y'], 'index')
+
+    graph = hv.Graph(((source_edges, target_edges), nodes)).opts(
+                        node_color='mean_deviation', cmap='viridis',
+                        node_size = 'node_sizes',
+                        tools=[hover], 
+                        hooks=[hook_graph],
+                        height = 600,
+                        responsive = True,
+                        xaxis = None, yaxis=None)
+    
+    return graph * labels
+
+def fix_x_axis(plot, element):
+    
+    fig = hv.render(plot)
+    fig.x_range=Range1d(0,1)
+
+def get_outlier_analysis(batches, output_directory, filename='ttmap'):
+      
+    # plot the number of modified genes by control sample 
+    if batches == []:
+        batches = ['All']
+    
+    total_number_of_batches = range(len(batches))
+
+    table_with_all_samples = pd.DataFrame({})
+
+    for batch in total_number_of_batches:
+        path_to_file = os.path.join(output_directory, filename + 'batch' +
+                                    str(batch) + '_na_numbers_per_col.txt')
+
+        dataset = pd.read_csv(path_to_file, header=0, index_col=0, sep='\t')
+        dataset['Batches'] = [batches[batch]] * dataset.shape[0]
+        dataset['Samples'] = list(dataset.index)
+
+        table_with_all_samples = pd.concat([table_with_all_samples, dataset])
+
+    table_with_all_samples.columns = ['Counts', 'Batches', 'Samples']
+
+    key_dimensions = [('Samples', 'Sample')]
+    value_dimensions = [('Counts', '# modified genes'), ('Batches', 'Batch')]
+
+    viridis_cmap = process_cmap("Viridis", provider="bokeh", ncolors=len(batches))
+    
+    number_modified_genes = hv.Bars(table_with_all_samples, key_dimensions, value_dimensions).opts(tools=['hover'],
+                                                                                   color='Batches',
+                                                                                   cmap=viridis_cmap,
+                                                                                   xticks = 0,
+                                                                                   legend_position='top_left',
+                                                                                   width = 500,
+                                                                                   responsive = True
+                                                                                  )
+    
+    # each gene is modified in x control samples. The plot below shows
+    # the frequency of the percentage of corrected values (by samples) per
+    # gene
+
+    grid_corrected_genes = pn.GridSpec(sizing_mode='stretch_both', width=500)
+    length_in_grid = np.int(np.ceil(np.float(len(batches))/np.float(3)))
+    
+    for counter, batch in enumerate(total_number_of_batches):
+        path_to_file = os.path.join(output_directory, filename + 'batch' +
+                                    str(batch) + '_na_numbers_per_row.txt')
+
+        dataset = pd.read_csv(path_to_file, header=0, index_col=0, sep='\t')
+
+        dataset.columns = ['Frequency', 'Other']
+
+        frequencies, edges = np.histogram(dataset['Frequency'], 4, range=(0,1))
+        
+        histogram_frequencies = hv.Histogram((edges, frequencies), extents = (0,0,1,None)).opts(xlim=(0,1),
+                                                                        responsive=True,
+                                                                        tools=['hover'],
+                                                                        hooks=[fix_x_axis],
+                                                                        width = 220,
+                                                                        title = batches[batch],
+                                                                        xlabel = '% of corrected genes',
+                                                                       )
+        
+        index_column = np.unravel_index(counter, [length_in_grid, 2])
+
+        grid_corrected_genes[index_column[0], index_column[1]] = histogram_frequencies
+    
+    grid_corrected_genes[length_in_grid, :] = number_modified_genes
+    
+    return grid_corrected_genes 
 
 
 def get_ttmap_from_inputs(event):
@@ -250,11 +421,139 @@ def query_clusters(event):
         grid_spec[1][3][2] = hv.Table(significant_genes_table.iloc[true_rows, :]).opts(width=1000, hooks=[fix_size_table])
         grid_spec[1][3].sizing_mode = 'stretch_both'
 
+margin_size = 5
+# widgets for the first column
+# widget to load matrix
+load_input  = pn.widgets.FileInput(name = 'Dataset input', margin=margin_size)
+# widget to type the batches
+which_batch = pn.widgets.TextInput(name = 'Batch names',
+                                   value = '', margin=margin_size)
+# widget to type the alpha value
+alpha_value = pn.widgets.TextInput(name = 'Alpha value', value = '1.0', margin=margin_size)
+# widget to type the output parameter
+outlier_parameter = pn.widgets.TextInput(name = 'Outlier parameter', value = '0', margin=margin_size)
+# widget to type the subselected test sample dataset
+test_samples = pn.widgets.TextInput(name = 'Test samples', value = '', margin=margin_size)
+# widget for the button to run the web app
+button_to_calculate = pn.widgets.Button(name = 'Calculate', button_type = 'primary', margin=margin_size)
+
+# widget to query for sample or cluster information
+query_box = pn.widgets.TextInput(name  = 'Search by sample or cluster number', 
+                                 value = '')
+# widget to select either sample or cluster
+query_dropdown = pn.widgets.Select(name    = 'Parameter to search in the dataframe',
+                                   options = ['Sample', 'Clusters'])
+
+# widget to query by cluster
+query_box_deviated_genes = pn.widgets.TextInput(name  = 'Insert cluster index here', 
+                                                value = '')
+# widget to type the specific genes to query
+query_genes = pn.widgets.TextInput(name  = 'Insert gene or list of genes separated by comma', 
+                                                value = '')
+
 button_to_calculate.param.watch(get_ttmap_from_inputs, parameter_names='clicks')
 query_dropdown.param.watch(query_columns, parameter_names='value')
 query_box.param.watch(query_columns, parameter_names='value')
 query_box_deviated_genes.param.watch(query_clusters, parameter_names='value')
 query_genes.param.watch(query_clusters, parameter_names='value')
+
+how_to_use = """
+# ttmap 
+---
+
+A web-app version of [Two-Tier Mapper](https://www.ncbi.nlm.nih.gov/pubmed/30753284)
+to analyse gene expression levels.
+
+## Basic instructions 
+---
+
+### Input
+
+In order to use TTMapp, you need to upload a matrix with a matrix containing your data. The format of the 
+matrix is given below.
+
+|  | Sample1 | Sample2 | ... | Sample3 |
+|--:|:------------:|:----------:|:-----:|:----------:|
+| Gene1| x11| x12  | ... |x1N |
+| Gene2| x21| x22 | ...  |x2N |
+| ...| 
+| GeneM |xM1| xM2 | ... |xMN |
+
+This matrix should follow some specific rules: 
+
+- The samples representing the control group should have CTRL in its name. It can be lower case too. 
+- The batches should be identified in the colum names also. If Sample1 is from batch X and Sample2 
+  is from batch Y, their names should contain X and Y respectively. 
+
+### Batch names
+The batches need to be specified if you want to batch correct. If you have 2 batches, X and Y,
+it should be given as X,Y in the box. 
+
+### Outlier parameter
+When performing the control adjusment, Two-Tier Mapper calculates how distant a control sample is from 
+the median of samples in the same batch. If this difference is bigger than a threshold, then
+its value is corrected. This threshold is given by the *Outlier parameter*. The default is 1. 
+
+### Alpha value
+A threshold is necessary to filter noise when calculating the mismatch distance. The alpha
+value is this threshold. The bigger the value, the less noise you allow. The default is 1.
+
+### Test samples
+If your matrix contains several columns and you want to analyse just a subset of them, you can
+give a name in which represents these columns. For example, suppose you have a matrix with 10 columns
+whose names are:
+
+- CTRL_1_batch1, CTRL_2_batch1, CTRL_3_batch2
+- Treat1_1_batch1, Treat1_2_batch1, Treat1_1_batch2,Treat1_2_batch2
+- Treat2_1_batch1, Treat2_2_batch1, Treat2_1_batch2,Treat2_2_batch2
+
+Then, to run TTMapp only on the samples from Treat1, you can type Treat1 in the Test Samples Box.
+
+### Running TTMapp
+After loading and setting these variables, you can press the button "Calculate" to run the 
+app.
+
+## Output
+---
+There are several outputs, and they are spread in the tabs *Two-Tier Mapper*, *Outlier Analysis*
+and *Significant Components*. 
+
+### Two - Tier Mapper
+The main output of TTMapp is in the tab *Two-Tier Mapper*. There you can find the graph and a table. 
+
+The graph is divided into 5 rows. From bottom to up, the first row is the overall interval, where
+all samples are analysed together. The second row corresponds to the low quartile, were samples
+with small total absolute deviation are clustered. In the third, fourth and fifth row there are
+the 2nd quartile, 3rd quartile and high quartile respectively, where the total absolute deviation
+increases from one quartile to the other. 
+
+The table is a representantion of the graph, where you can search either by sample or cluster. It has
+the same kind of information, but in a tabular manner.
+
+### Outlier Analysis
+In this tab, there are two plots. One containing the frequency that the genes were 
+corrected in the control adjustment step divided by batch effects.
+
+The other plot shows the number of corrected genes by sample.
+
+These plots are good to look for outliers in your data, given that a sample could have more 
+modified genes than the rest or some genes were modified in all samples, meaning they deviate
+too much and could harm the analysis. 
+
+### Significant Components
+Each cluster has an associated set of genes, those that deviate in the same direction with respect
+to the samples in the cluster. Deviating in the same direction means that the deviation components 
+for each gene have the same sign. 
+
+In this tab you can filter by cluster and then by gene. To select a cluster, you can type the index
+that is in the corresponding node of the graph. Also, you can type a list of genes separated
+by comma, e.g., "GREB1, BRCA1". Note that the genes should be the same as in the matrix you provided
+at the beginning of the analysis.
+
+## Future additions
+---
+- Download all data produced
+"""
 
 how_to_use_it = pn.Column(pn.pane.Markdown(how_to_use, sizing_mode='stretch_both'),
                           sizing_mode='stretch_both', scroll = True)
